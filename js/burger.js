761c44941aee0764bbcbea632964b1cf7f6f0010
Sergei Tulupov <citybox1@yandex.ru>
2019-09-04 17:59:10 +0300

    slide menu is done

--- js/burger.js
+++ js/burger.js
@@ -1,17 +1,16 @@
 function handler(){
 let tst=document.querySelector(".side_panel");
-tst.style.display="block";
+tst.style.left=0;
 };
 
 function handler2(){
     let tst=document.querySelector(".side_panel");
-    tst.style.display="none";
+    tst.style.left="-320px";
     };
 
-
-burg.addEventListener('click',handler);
+    burg.addEventListener('click',handler);
 burg2.addEventListener('click',handler2);
 window.addEventListener(`resize`, event => {
     let tst=document.querySelector(".side_panel");
-    tst.style.display="none";
+    tst.style.left="-320px";
   }, false);
